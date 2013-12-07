d730cc1b676652443a007528697ae4035b421821
Gelx <gelx@lerozon.net>
2013-12-07 11:45:07 +0100

    Add main class and plugin.yml

---
+++ Location/src/net/lerozon/plugins/location/Location.java
@@ -0,0 +1,31 @@
+package net.lerozon.plugins.location;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Location extends JavaPlugin implements Listener{
+	
+	public void onEnable(){
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
+		
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerInteract(PlayerInteractEvent evt){
+		if(evt.getMaterial() == Material.PAPER){
+			evt.getPlayer().sendMessage(getStringFromLocation(evt.getClickedBlock().getLocation()));
+		}
+	}
+	
+	public static String getStringFromLocation(org.bukkit.Location in){
+		return in.getWorld() + ":" + in.getX() + "," + in.getY() + "," + in.getBlockZ() + "," +
+			   in.getYaw() + "," + in.getPitch();
+	}
+	
+}
